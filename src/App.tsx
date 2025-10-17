066ccea3e410dfd446803d5178e0d5649bd9c713
Felipe Lucero <felipelucerocosta@gmail.com>
2025-10-17 19:53:56 -0300

    cambios a ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import styles from "./App.module.css";
 
 // 👇 Importa respetando mayúsculas y carpetas
 import Registro from "../src/componentes/registro";
@@ -7,7 +6,7 @@ import Clases from "./componentes/clases";
 import GestionClase from "./componentes/trabajosenclase";
 import Foro from "./componentes/foro";
 import Calendario from "./componentes/calendario";
-// 👇 Como GaleriaSimulaciones es export default se importa SIN llaves
+
 import GaleriaSimulaciones from "./componentes/simulaciones"; 
 // Vistas para alumno
 import ClasesAlumno from "./comoponentesalumno/clasesalumno";
@@ -15,21 +14,19 @@ import TrabajosAlumno from "./comoponentesalumno/trabajosalumno";
 
 function App() {
   return (
-    <div className={styles.mainContainer}>
-      <Router>
-        <Routes>
+    <Router>
+      <Routes>
         <Route path="/" element={<Registro />} />
         <Route path="/clases" element={<Clases />} />
         <Route path="/gestionClase" element={<GestionClase />} />
-  {/* Rutas para vistas de alumno */}
-  <Route path="/alumno" element={<ClasesAlumno />} />
-  <Route path="/alumno/gestion" element={<TrabajosAlumno />} />
+        {/* Rutas para vistas de alumno */}
+        <Route path="/alumno" element={<ClasesAlumno />} />
+        <Route path="/alumno/gestion" element={<TrabajosAlumno />} />
         <Route path="/foro" element={<Foro />} />
         <Route path="/calendario" element={<Calendario />} />
         <Route path="/simulaciones" element={<GaleriaSimulaciones />} />
-        </Routes>
-      </Router>
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
