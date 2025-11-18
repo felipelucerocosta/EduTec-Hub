0a64f5aad4835a3c5ea0833f47442b1aba806a96
felipelucerocosta <163046155+felipelucerocosta@users.noreply.github.com>
2025-11-18 20:08:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/EduTec-Hub/",   // 👈 IMPORTANTE PARA GITHUB PAGES
   plugins: [react()],
-})
+});
